5284dbd7b5c66ed035843eb1a01e34dac458c2df
NhanTruong <truongchinhan2010@gmail.com>
2022-07-15 18:34:48 +0700

    First Commit
    First Commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DMCTimesheet.Models;
 
 namespace DMCTimesheet.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly dmcDbcontext db = new dmcDbcontext();
         public ActionResult Index()
         {
+
             return View();
         }
 
